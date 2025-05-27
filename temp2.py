8d7d0dd7a46f04307d345146f2fe4b204b03f67d
LE JIANG <lejiang@LEdeMacBook-Air.local>
2025-05-27 15:42:55 +0900

    更新 hough  文件

--- temp2.py
+++ temp2.py
@@ -1,31 +1,26 @@
-if __name__ == "__main__":
-    os.makedirs("output2", exist_ok=True)
-    os.makedirs("output3", exist_ok=True)
-    detector = ProbabilisticHough()
+def preprocess_image(self, image: Image.Image) -> Image.Image:
+    cv_image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
+    gray = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
 
-    image_paths = glob.glob("input1/*.jpg")  
-    for path in image_paths:
-        image = Image.open(path)
-        
-        endpoints = detector.detect_points(image)
-        processed_image = detector.preprocess_image(image)
+    thresh = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+                                   cv2.THRESH_BINARY_INV, 
+                                   11,  # 小さいほど細かく検出出来る（奇数）
+                                   2   # 大きほと白になりにくい
+                                   )
 
-        img_bgr = cv2.cvtColor(np.array(processed_image), cv2.COLOR_RGB2BGR)
-        H, W = img_bgr.shape[:2]
-        cx, cy = W // 2, H // 2
+    contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    H = cv_image.shape[0]
+    top_threshold = int(H * 0.2)
+    bottom_threshold = int(H * 0.8)
 
-        if endpoints:
-            print(f"{path} → 检测到 {len(endpoints)} 组端点")
-            for idx, (pt1, pt2) in enumerate(endpoints):
-                print(f"  组 {idx + 1}: {pt1}, {pt2}")
-                x1, y1 = int(cx + pt1[0]), int(cy - pt1[1])
-                x2, y2 = int(cx + pt2[0]), int(cy - pt2[1])
-                cv2.circle(img_bgr, (x1, y1), detector.dot_radius, (255, 0, 0), -1)
-                cv2.circle(img_bgr, (x2, y2), detector.dot_radius, (0, 255, 0), -1)
+    for cnt in contours:
+        if cv2.contourArea(cnt) > 50:
+            # 获取轮廓的外接矩形
+            x, y, w, h = cv2.boundingRect(cnt)
+            center_y = y + h // 2
 
-        merged = cv2.hconcat([
-            cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR),
-            img_bgr  # 已在 processed_image 上绘制端点
-        ])        
-        save_path = os.path.join("output6", os.path.basename(path))
-        cv2.imwrite(save_path, merged)
+            # 只处理中间区域的轮廓（排除顶部20%和底部20%）
+            if top_threshold <= center_y <= bottom_threshold:
+                cv2.drawContours(cv_image, [cnt], -1, (0, 0, 255), thickness=cv2.FILLED)
+
+    return Image.fromarray(cv2.cvtColor(cv_image, cv2.COLOR_BGR2RGB))
